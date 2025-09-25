1370caf807d819ab76b95cd2d67b1c11c90573f7
Sheida Jahanfar <129843468+sheidajahanfar@users.noreply.github.com>
2025-09-25 19:24:25 +0200

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,10 +1,1 @@
--- View first 10 rows
-SELECT * FROM snappay LIMIT 10;
 
--- Count total users
-SELECT COUNT(*) AS total_users FROM snappay;
-
--- Count defaulters
-SELECT is_defaulter, COUNT(*) AS user_count
-FROM snappay
-GROUP BY is_defaulter;
